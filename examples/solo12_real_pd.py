0c4c19fb47c641713de2e13ca7f5cb5b666f59a0
Julian Viereck <jviereck@nyu.edu>
2021-07-14 16:52:27 -0400

    Add solo12_real_pd.py example

---
+++ examples/solo12_real_pd.py
@@ -0,0 +1,35 @@
+# This a demo to test the ilqr for 2d point mass
+import numpy as np
+
+from dynamic_graph_head import ThreadHead, HoldPDController
+
+import time
+
+import dynamic_graph_manager_cpp_bindings
+from robot_properties_solo.solo12wrapper import Solo12Config
+
+print("Finished imports")
+
+###
+# Create the dgm communication and instantiate the controllers.
+head = dynamic_graph_manager_cpp_bindings.DGMHead(Solo12Config.dgm_yaml_path)
+
+head.read()
+print(head.get_sensor('slider_positions'))
+
+# Create the controllers.
+hold_pd_controller = HoldPDController(head, 3., 0.05, with_sliders=True)
+
+thread_head = ThreadHead(
+    0.002,
+    hold_pd_controller,
+    head,
+    []
+)
+
+# Start the parallel processing.
+thread_head.start()
+
+print("Finished controller setup")
+
+input("Wait for input to finish program.")
